45a957b07cbdbaaaab990c9e2704972ec2ec5085
jamsheer <jamshibkl3@gmail.com>
2024-02-06 11:38:58 +0530

    added folder

---
+++ src/components/NavBar/NavBar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import './NavBar.css'
+function NavBar() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default NavBar
---
+++ src/pages/AboutUs/AboutUs.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import "./AboutUs.css"
+function AboutUs() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default AboutUs

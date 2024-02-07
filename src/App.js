599690f4e7a3085893e264270e62fc982d6efc8b
Kaushik N <kaushik1603n@gmail.com>
2024-02-07 12:39:45 +0530

    footer section completedd

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import './App.css';
-import AboutUs from './pages/AboutUs/AboutUs';
-import Footer from './components/Footer/Footer';
+// import AboutUs from './pages/AboutUs/AboutUs';
+// import Footer from './components/Footer/Footer';
 
 function App() {
   return (
     <div className="App">
      <h1>Voyago</h1>
-     <AboutUs/>
-     <Footer />
+     {/* <AboutUs/>
+     <Footer /> */}
     </div>
   );
 }

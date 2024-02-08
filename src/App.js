2ded446f7e68e1f4638871d76edbe3ffbece325f
Mohammed Jamsheer <jamshijass9895@gmail.com>
2024-02-09 00:08:32 +0530

    Service page added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import './App.css';
-import Home from '../src/pages/Home/Home'
+// import Home from '../src/pages/Home/Home'
 // import HowItWorks from './pages/HowItWorks/HowItWorks';
+import ServiceStatus from './pages/ServiceStatus/ServiceStatus';
 function App() {
   return (
     <div className="App">
-     <h1>Voyago</h1>
-     <Home />
+  <ServiceStatus />
+    
     </div>
   );
 }

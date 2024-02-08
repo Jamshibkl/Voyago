d1211d50ae8eeb86be45772a90e57291845409c9
Mohammed Jamsheer <jamshijass9895@gmail.com>
2024-02-09 00:12:23 +0530

    Service Status added

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
 // import Home from '../src/pages/Home/Home'
 // import HowItWorks from './pages/HowItWorks/HowItWorks';
-import ServiceStatus from './pages/ServiceStatus/ServiceStatus';
+// import ServiceStatus from './pages/ServiceStatus/ServiceStatus';
 function App() {
   return (
     <div className="App">

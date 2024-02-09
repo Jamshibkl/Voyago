433f8ca5cbacef5f4c45ad9bc4005819d0f115b4
Kaushik N <kaushik1603n@gmail.com>
2024-02-09 12:15:43 +0530

    our team section compleated

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
-import './App.css';
-import Home from '../src/pages/Home/Home'
+import "./App.css";
+import Home from "../src/pages/Home/Home";
 // import HowItWorks from './pages/HowItWorks/HowItWorks';
 function App() {
   return (
     <div className="App">
-   
-     <Home />
+      <Home />
     </div>
   );
 }

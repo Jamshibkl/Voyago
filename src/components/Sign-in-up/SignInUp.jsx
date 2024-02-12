48685fa74b9304b1eaa8aa660d32cb0bc7354938
Kaushik N <kaushik1603n@gmail.com>
2024-02-12 19:27:07 +0530

    login page modifyed

--- src/components/Sign-in-up/SignInUp.jsx
+++ src/components/Sign-in-up/SignInUp.jsx
@@ -1,7 +1,6 @@
-import React from "react";
-import { useState } from "react";
+import React, { useState } from "react";
+import Navbar  from "../NavBar/NavBar";
 import "./SignInUp.css";
-import NavBar from "../NavBar/NavBar";
 
 function SignInUp() {
   const [isActive, setIsActive] = useState(false);
@@ -13,60 +12,63 @@ function SignInUp() {
   const handleLoginClick = () => {
     setIsActive(false);
   };
-
   return (
-    <div className="main-container-sign">
-      <div className={`container1 ${isActive ? "active" : ""}`}>
-        <div className="form-container sign-up">
-          <form>
-            <h1>Create Account</h1>
-            <div className="social-icons">
-              <a href="#" className="icon"></a>
-              <a href="#" className="icon"></a>
-              <a href="#" className="icon"></a>
-              <a href="#" className="icon"></a>
-            </div>
-            <span>or use your email for registration</span>
-            <input type="text" placeholder="Name" />
-            <input type="email" placeholder="Email" />
-            <input type="password" placeholder="Password" />
-            <button onClick={handleRegisterClick}>Sign Up</button>
-          </form>
-        </div>
+    <div>
+      <Navbar/>
+      <div className="main-container-sign">
+        <div className={`container1 ${isActive ? "active" : ""}`}>
+          <div className="form-container sign-up">
+            <form>
+              <h1>Create Account</h1>
+              <div className="social-icons">
+                <a href="#" className="icon"></a>
+                <a href="#" className="icon"></a>
+                <a href="#" className="icon"></a>
+                <a href="#" className="icon"></a>
+              </div>
+              <span>or use your email for registration</span>
+              <input type="text" placeholder="Name" />
+              <input type="email" placeholder="Email" />
+              <input type="password" placeholder="Password" />
+              <button onClick={handleRegisterClick}>Sign Up</button>
+            </form>
+          </div>
 
-        <div className="form-container sign-in">
-          <form>
-            <h1>Sign In</h1>
-            <div className="social-icons">
-              <a href="#" className="icon"></a>
-              <a href="#" className="icon"></a>
-              <a href="#" className="icon"></a>
-              <a href="#" className="icon"></a>
-            </div>
-            <span>or use your email password</span>
-            <input type="email" placeholder="Email" />
-            <input type="password" placeholder="Password" />
-            <a href="#">Forget Your Password?</a>
-            <button onClick={handleLoginClick}>Sign In</button>
-          </form>
-        </div>
-        <div className="toggle-container">
-          <div className="toggle">
-            <div className="toggle-panel toggle-left">
-              <h1>Welcome Back!</h1>
-              <p>Enter your personal details to use all of site features</p>
-              <button className="hidden" onClick={handleLoginClick}>
-                Sign In
-              </button>
-            </div>
-            <div className="toggle-panel toggle-right">
-              <h1>Hello, Friend!</h1>
-              <p>
-                Register with your personal details to use all of site features
-              </p>
-              <button className="hidden" onClick={handleRegisterClick}>
-                Sign Up
-              </button>
+          <div className="form-container sign-in">
+            <form>
+              <h1>Sign In</h1>
+              <div className="social-icons">
+                <a href="#" className="icon"></a>
+                <a href="#" className="icon"></a>
+                <a href="#" className="icon"></a>
+                <a href="#" className="icon"></a>
+              </div>
+              <span>or use your email password</span>
+              <input type="email" placeholder="Email" />
+              <input type="password" placeholder="Password" />
+              <a href="#">Forget Your Password?</a>
+              <button onClick={handleLoginClick}>Sign In</button>
+            </form>
+          </div>
+          <div className="toggle-container">
+            <div className="toggle">
+              <div className="toggle-panel toggle-left">
+                <h1>Welcome Back!</h1>
+                <p>Enter your personal details to use all of site features</p>
+                <button className="hidden" onClick={handleLoginClick}>
+                  Sign In
+                </button>
+              </div>
+              <div className="toggle-panel toggle-right">
+                <h1>Hello, Friend!</h1>
+                <p>
+                  Register with your personal details to use all of site
+                  features
+                </p>
+                <button className="hidden" onClick={handleRegisterClick}>
+                  Sign Up
+                </button>
+              </div>
             </div>
           </div>
         </div>

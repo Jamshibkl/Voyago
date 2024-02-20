ea28f0159b406d18b7ea7c69f8a50057146a4bab
Mohammed Jamsheer <jamshijass9895@gmail.com>
2024-02-21 00:57:53 +0530

    Book a Driver completed

--- src/App.js
+++ src/App.js
@@ -9,14 +9,14 @@ import HowItWorks from "./pages/HowItWorks/HowItWorks";
 import Safety from "./pages/Safety/Safety";
 import ServiceStatus from "./pages/ServiceStatus/ServiceStatus";
 import LeagalPrivacy from "./pages/LeagalAandPrivacy/LeagalPrivacy";
-import BookADriver from "./components/Book-a-Driver/BookADriver"
+import BookADriver from "./components/Book-a-Driver/Booking-form/BookADriver"
+import DriverOnWay from "./components/Book-a-Driver/Driver-on-way/DriverOnWay";
+import Destination from "./components/Book-a-Driver/Destination/Destination";
+import OnTheWay from "./components/Book-a-Driver/OnTheWay/OnTheWay";
+import PaymentSection from "./components/Book-a-Driver/PaymentSection/PaymentSection";
+import DriverConform from "./components/Book-a-Driver/Driver-conformed/DriverConform";
 import ContactUs from "./pages/ContactUS/ContactUs";
 import SignInUp from "./components/Sign-in-up/SignInUp";
-import Local from "./components/Booking-form/Local/Local"
-import Outstation from "./components/Booking-form/Outstation/Outstation";
-import Permanent from "./components/Booking-form/Permanent/Permanent";
-import DriverSelection from "./components/Booking-form/DriverSelection/DriverSelection";
-import PaymentSection from "./components/Booking-form/PaymentSection/PaymentSection";
 function App() {
   return (
     <div className="App">
@@ -31,13 +31,12 @@ function App() {
           <Route path="/service-status" element={<ServiceStatus />} />
           <Route path="/leagal-privacy" element={<LeagalPrivacy />} />
           <Route path="/book-a-driver" element={<BookADriver />} />
+          <Route path="/driver-confirm" element={<DriverConform />} />
+          <Route path="/driver-on-way" element={<DriverOnWay />} />
           <Route path="/contact" element={<ContactUs />} />
           <Route path="/sign-in" element={<SignInUp />} />
-          <Route path="/sign-up" element={<SignInUp />} />
-          <Route path="/local" element={<Local />} />
-          <Route path="/outstation" element={<Outstation />} />
-          <Route path="/permanent" element={<Permanent />} />
-          <Route path="/driver-selection" element={<DriverSelection />} />
+          <Route path="/on-the-way" element={<OnTheWay />} />
+          <Route path="/destination" element={<Destination />} />
           <Route path="/payment-section" element={<PaymentSection />} />
         </Routes>
       </Router>
---
+++ src/components/Book-a-Driver/Booking-form/BookADriver.jsx
@@ -0,0 +1,88 @@
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import "./BookADriver.css";
+import BookingImage from "../../../Assets/Study abroad-rafiki.svg";
+import NavBar from "../../../components/NavBar/NavBar";
+
+// import Footer from '../../components/Footer/Footer'
+function BookADriver() {
+  const [pickupDate, setPickupDate] = useState("");
+  const [pickupTime, setPickupTime] = useState("");
+
+  const handleDateFocus = () => {
+    document.getElementById("pickupDate").type = "date";
+  };
+
+  const handleDateBlur = () => {
+    if (!pickupDate) {
+      document.getElementById("pickupDate").type = "text";
+    }
+  };
+
+  const handleTimeFocus = () => {
+    document.getElementById("pickuptime").type = "time";
+  };
+
+  const handleTimeBlur = () => {
+    if (!pickupTime) {
+      document.getElementById("pickuptime").type = "text";
+    }
+  };
+
+  return (
+    <>
+      <NavBar />
+      <div className="book-ride-section">
+        <div className="booking-form">
+          <h1>Conform the Ride</h1>
+          <form action="">
+            <input type="text" placeholder="Pick up Location" />
+
+            <input type="text" placeholder="Drop Location" />
+
+            <br />
+            <input
+              placeholder="Pick up Date"
+              name="pickupDate"
+              id="pickupDate"
+              value={pickupDate}
+              onFocus={handleDateFocus}
+              onBlur={handleDateBlur}
+              onChange={(e) => setPickupDate(e.target.value)}
+            />
+
+            <input
+              name="pickuptime"
+              id="pickuptime"
+              value={pickupTime}
+              placeholder="Pick Up Time"
+              onFocus={handleTimeFocus}
+              onBlur={handleTimeBlur}
+              onChange={(e) => setPickupTime(e.target.value)}
+            />
+
+            <br />
+            <select>
+              <option value="">Cab Type</option>
+              <option value="">Luxury</option>
+              <option value="">SUV</option>
+              <option value="">Innova</option>
+              <option value="">Sedan</option>
+            </select>
+          </form>
+          <Link to="/driver-on-way">
+            <button className="booking-btn">Book a Ride</button>
+          </Link>
+          <Link to="/">
+            <button className="back-to-home-btn">Go Back</button>
+          </Link>
+        </div>
+        <div className="booking-image">
+          <img src={BookingImage} alt="book img" />
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default BookADriver;

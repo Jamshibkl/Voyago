eddce497df8ec8574a4b2d91ee3bca4d8d28f19d
Kaushik N <kaushik1603n@gmail.com>
2024-03-06 13:00:18 +0530

    a

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import "./App.css";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+
 import Home from "./pages/Home/Home";
 import AboutUs from "./pages/AboutUs/AboutUs";
 import OurTeam from "./pages/OurTeam/OurTeam";
